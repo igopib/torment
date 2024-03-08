a3ddca5d83dd644a5966dcba1dae92496da2bfd6
Gurpreet <gs109690@gmail.com>
2024-03-07 22:59:01 -0600

    feat: next app with required packages

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='w-full h-screen bg-[#0006b1] text-white'>
+      <div className='h-full grid text-justify w-full'>
+        <h1 className='font-black uppercase w-full text-justify'>Torment</h1>
+      </div>
+    </div>
+  )
+}
