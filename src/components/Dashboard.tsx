687798e40af3470ee1b83b629fa05a15e981c3fb
Gurpreet <gs109690@gmail.com>
2024-03-12 18:58:12 -0500

    refactor: variable colors used

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,7 +3,7 @@ import { LOGO } from '@/lib/ascii'
 
 export default function Dashboard() {
   return (
-    <div className='bg-[#0006b1] p-2 min-h-screen' style={{ fontFamily: 'Ibm' }}>
+    <div className='p-2 min-h-screen' style={{ fontFamily: 'Ibm' }}>
       <div className='h-full flex gap-4 border border-dashed flex-col text-start justify-start p-2'>
         <pre className='text-[0.30rem] sm:text-[0.6rem] text-center'>
           <code>{LOGO}</code>
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,10 +1,10 @@
 export default function MenuBar() {
   return (
     <div className='flex justify-between sm:text-base sm:justify-evenly'>
-      <span className='hover:text-[#0006b1] hover:bg-white cursor-pointer duration-100'>• ???? •</span>
-      <span className='hover:text-[#0006b1] hover:bg-white cursor-pointer duration-100'>• ???? •</span>
-      <span className='hover:text-[#0006b1] hover:bg-white cursor-pointer duration-100'>• ???? •</span>
-      <span className='hover:text-[#0006b1] hover:bg-white cursor-pointer duration-100'>• ???? •</span>
+      <span className='hover:text-background hover:bg-foreground cursor-pointer duration-100'>• ???? •</span>
+      <span className='hover:text-background hover:bg-foreground cursor-pointer duration-100'>• ???? •</span>
+      <span className='hover:text-background hover:bg-foreground cursor-pointer duration-100'>• ???? •</span>
+      <span className='hover:text-background hover:bg-foreground cursor-pointer duration-100'>• ???? •</span>
     </div>
   )
 }
